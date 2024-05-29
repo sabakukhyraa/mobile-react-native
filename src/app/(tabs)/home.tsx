70e2d4efa9430a2fc94ec0dabe15bea11d719376
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2024-05-29 23:17:54 +0300

    home page content update & image

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,17 +1,12 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/src/components/EditScreenInfo';
+import { Image, StyleSheet } from 'react-native';
 import { Text, View } from '@/src/components/Themed';
-import { Link } from 'expo-router';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-      <Link style={styles.link} href="/login">Login</Link>
-      <Link style={styles.link} href="/register">Register</Link>
+      <Text style={styles.title}>Welcome to the Todo App</Text>
+      <Image style={styles.image} source={require('@/assets/images/crocodile.png')} />
+      <Text style={styles.text}>This is a Todo app developed by <Text style={styles.highlighted}>Ali Kerem Ata</Text> for educative reasons. But you can use it when you go to grocery shopping if you want to!</Text>
     </View>
   );
 }
@@ -20,18 +15,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingHorizontal: 32,
   },
   title: {
-    fontSize: 20,
+    marginTop: '25%',
+    textAlign: 'center',
+    fontSize: 45,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  text: {
+    marginTop: 24,
+    textAlign: 'center',
   },
-  link: {
-    color: "#ffffff"
+  highlighted: {
+    fontWeight: '900',
+    fontSize: 16
+  },
+  image: {
+    marginTop: 24,
+    resizeMode: 'contain',
+    width: '100%',
+    height: '40%',
   }
 });
