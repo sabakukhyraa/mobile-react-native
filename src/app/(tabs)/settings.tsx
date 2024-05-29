0c4fb85f2701e7d43b669f999cc7948404d58aeb
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2024-05-29 23:18:16 +0300

    Alert ensure log out

--- src/app/(tabs)/settings.tsx
+++ src/app/(tabs)/settings.tsx
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet } from 'react-native';
+import { Alert, Image, Pressable, StyleSheet } from 'react-native';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import Colors from '@/src/constants/Colors';
 import { useColorScheme } from '@/src/components/useColorScheme';
@@ -6,6 +6,14 @@ import { Text, View } from '@/src/components/Themed';
 import { FIREBASE_AUTH } from '@/Firebase';
 export default function SettingsScreen() {
   const colorScheme = useColorScheme();
+
+  const logout = () => {
+    const alertResponse = Alert.alert('Question', 'Are you sure you want to log out?', [
+      { text: 'Cancel', onPress: () => { } },
+      { text: 'Sure', onPress: () => FIREBASE_AUTH.signOut() }
+    ])
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.listSection}>
@@ -14,17 +22,17 @@ export default function SettingsScreen() {
           onPress={() => { }}
         >
           {({ pressed }) => (
-            <>
+            <View style={styles.listItemInside}>
               <FontAwesome
                 name="sticky-note"
                 size={25}
                 color={Colors[colorScheme ?? 'light'].text}
                 style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
               />
-              <Text>
+              <Text style={styles.listText}>
                 Something
               </Text>
-            </>
+            </View>
           )}
         </Pressable>
         <Pressable
@@ -32,37 +40,38 @@ export default function SettingsScreen() {
           onPress={() => { }}
         >
           {({ pressed }) => (
-            <>
+            <View style={styles.listItemInside}>
               <FontAwesome
                 name="trash"
                 size={25}
                 color={Colors[colorScheme ?? 'light'].text}
                 style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
               />
-              <Text>
+              <Text style={styles.listText}>
                 Delete All Data
               </Text>
-            </>
+            </View>
           )}
         </Pressable>
         <Pressable
           style={styles.listItem}
-          onPress={() => FIREBASE_AUTH.signOut()}
+          onPress={logout}
         >
           {({ pressed }) => (
-            <>
+            <View style={styles.listItemInside}>
               <FontAwesome
                 name="sign-out"
                 size={25}
                 color={Colors[colorScheme ?? 'light'].text}
                 style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
               />
-              <Text>
+              <Text style={styles.listText}>
                 Log out
               </Text>
-            </>
+            </View>
           )}
         </Pressable>
+        <Image style={styles.image} source={require('@/assets/images/surfing.png')} />
       </View>
     </View>
   );
@@ -79,6 +88,23 @@ const styles = StyleSheet.create({
   },
   listItem: {
     color: 'white',
-    paddingLeft: 16,
-  }
+    paddingHorizontal: 36,
+    paddingVertical: 16,
+    borderBottomWidth: .5,
+    borderColor: 'white',
+  },
+  listItemInside: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  listText: {
+    fontSize: 20,
+  },
+  image: {
+    marginTop: '25%',
+    resizeMode: 'contain',
+    width: '100%',
+    height: '45%',
+  },
 });
